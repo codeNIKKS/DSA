ecb8ca552e035a09f207bcc7d131d942ac234004
coderNekki <nikhilsiwach2006@gmail.com>
2026-05-17 18:27:45 +0530

    Added brute force solution

---
+++ Arrays/CheckSortedRotated.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+using namespace std;
+// PROBLEM:
+// Check if Array Is Sorted and Rotated
+
+// BRUTE FORCE APPROACH
+// Try every rotation, create rotated array,
+// then check if sorted.
+//
+// TC: O(n²)
+// SC: O(n)
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++) {
+
+            int index = 0;
+
+            for(int j = i; j < n; j++) {
+                arr[index++] = nums[j];
+            }
+
+            for(int j = 0; j < i; j++) {
+                arr[index++] = nums[j];
+            }
+
+            int k;
+
+            for(k = 0; k < n - 1; k++) {
+
+                if(arr[k] > arr[k + 1]) {
+                    break;
+                }
+            }
+
+            if(k == n - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
