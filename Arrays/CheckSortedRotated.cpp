a12df4364dfb205de8b9c8a2c335d9f08d7ce98b
Nikhil Siwach <163203416+codeNIKKS@users.noreply.github.com>
2026-05-17 21:44:14 +0530

    Refactor checkSortedRotated function
    Refactor check function to improve readability and efficiency.

--- Arrays/CheckSortedRotated.cpp
+++ Arrays/CheckSortedRotated.cpp
@@ -13,36 +13,27 @@ using namespace std;
 class Solution {
 public:
     bool check(vector<int>& nums) {
-
         int n = nums.size();
         vector<int> arr(n);
-
         for(int i = 0; i < n; i++) {
-
             int index = 0;
-
             for(int j = i; j < n; j++) {
                 arr[index++] = nums[j];
             }
-
             for(int j = 0; j < i; j++) {
                 arr[index++] = nums[j];
             }
-
             int k;
-
             for(k = 0; k < n - 1; k++) {
 
                 if(arr[k] > arr[k + 1]) {
                     break;
                 }
             }
-
             if(k == n - 1) {
                 return true;
             }
         }
-
         return false;
     }
 };
